dbe402146d9705999a527bec7dc877be33c61708
rubens Rodrigues <rubens@schoolguardian.app>
2022-06-04 11:31:23 -0300

    Class 1 - Section 3 - State & Props

--- src/Screens/Home/HomeController.tsx
+++ src/Screens/Home/HomeController.tsx
@@ -2,18 +2,35 @@ import React from "react";
 import HomeModel from "./HomeModel";
 import HomeView from "./HomeView";
 
-class HomeController extends React.Component {
+interface IState {
+  count: number;
+}
+
+class HomeController extends React.Component<any, IState> {
   homeModel: HomeModel;
 
   constructor() {
     super({});
 
+    this.state = {
+      count: 0,
+    };
+
     this.homeModel = new HomeModel(); //inicializando o view
     this.homeModel.getSomeInfo();
   }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        count: this.state.count + 1,
+      });
+    }, 1000);
+  }
+
   render() {
     return (
-      <HomeView /> //Chamando o View
+      <HomeView info={this.state.count} /> //Chamando o View
     );
   }
 }
--- src/Screens/Home/HomeView.tsx
+++ src/Screens/Home/HomeView.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Home.css";
 
-function HomeView() {
-  return <div className="App">Hello World </div>;
+interface IProps {
+  info: number;
 }
+const HomeView: React.FC<IProps> = (props) => {
+  return (
+    <div className="container">
+      <h1 className="text">Info: {props.info}</h1>
+    </div>
+  );
+};
 
 export default HomeView;
