35a136e1746afdf4f1dbaac649773cba08b5218b
rubens Rodrigues <rubens@schoolguardian.app>
2022-06-05 15:01:10 -0300

    Class 2 - Section 2 - Styled Components

--- src/Screens/Home/HomeView.tsx
+++ src/Screens/Home/HomeView.tsx
@@ -1,8 +1,8 @@
 import "./Home.css";
 import * as React from "react";
-import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
+import { Title, Box, AcUnitIconCustom, CustomBox } from "./HomeStyle";
 
 interface IProps {
   info: number;
@@ -10,24 +10,27 @@ interface IProps {
 
 const HomeView: React.FC<IProps> = ({ info }) => {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      className="container"
-    >
-      <Typography gutterBottom variant="h1" color="primary.dark">
-        Info {info}
-      </Typography>
-      <Typography gutterBottom variant="h1" color="secondary.dark">
-        Info {info}
-      </Typography>
-      <Button color="primary"> Primary </Button>
-      <Button color="secondary"> Secondary </Button>
-      <Button color="success"> Success </Button>
-    </Grid>
+    <Box>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <AcUnitIconCustom />
+        <Title gutterBottom variant="h1" color="primary.dark">
+          Info {info}
+        </Title>
+        <Title gutterBottom variant="h1" color="secondary.dark">
+          Info {info}
+        </Title>
+        <CustomBox color="#fff000"></CustomBox>
+        <Button variant="primary"> Primary </Button>
+        <Button color="secondary"> Secondary </Button>
+        <Button color="success"> Success </Button>
+      </Grid>
+    </Box>
   );
 };
 
