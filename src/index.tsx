148ab6b809326cbeabbba64737c72533d4e0639e
rubens Rodrigues <rubens@schoolguardian.app>
2022-06-08 23:45:52 -0300

    Class 6 - PWA

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Routes/Routes";
 import MuiThemes from "./Utils/Common/MuiThemes";
-import UserInfoContextProvider from './Store/UserInfo/UserInfoProvider';
+import UserInfoContextProvider from "./Store/UserInfo/UserInfoProvider";
+import serviceWorker from "./serviceworker";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
@@ -27,3 +28,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.registerServiceWorker();
